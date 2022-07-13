36134e11b67f09f16968b1366c168e1f6a6bfadb
GeoViklander <58559888+GeoViklander@users.noreply.github.com>
2022-07-13 15:11:24 -0300

    usando regex

--- training/r7/index.js
+++ training/r7/index.js
@@ -3,8 +3,12 @@ function button() {
   var input = document.querySelector('[data-input]')
  
   $button.addEventListener('click', () => {
-    console.log(input.value.replace(/['"]+/g, ''))
+    console.log(input.value.replace(/[^a-zA-Z0-9_-]/g,''));
     });
   };
 
 button()
+
+
+//estudo
+// https://www.wake-up-neo.com/pt/javascript/como-substituo-caracteres-especiais-pelo-regex-em-javascript/942438546/
