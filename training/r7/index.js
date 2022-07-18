f8805cd6d33a956e379da69ab7b901d5305e4306
GeoViklander <58559888+GeoViklander@users.noreply.github.com>
2022-07-18 15:33:26 -0300

    substituindo aspas inglesas por duplas

--- training/r7/index.js
+++ training/r7/index.js
@@ -3,12 +3,15 @@ function button() {
   var input = document.querySelector('[data-input]')
  
   $button.addEventListener('click', () => {
-    console.log(input.value.replace(/[^a-zA-Z0-9_-]/g,''));
+    console.log(input.value.replace(/[“”]/g, '"'));
     });
   };
 
 button()
 
+//frase de exemplo
+//"Estou" l'en'do “Orgulho e Preconceito” de Jane Austen.
 
 //estudo
 // https://www.wake-up-neo.com/pt/javascript/como-substituo-caracteres-especiais-pelo-regex-em-javascript/942438546/
+//https://www.alura.com.br/artigos/javascript-replace-manipulando-strings-e-regex
